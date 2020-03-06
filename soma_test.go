fd1be1d8a257906d66c3526e3f5e478fbb3f9016
Rafael Rocha <rafael.rocha.mg@gmail.com>
2020-03-05 23:29:59 -0300

    Separação do test do arquivo principal

--- soma_test.go
+++ soma_test.go
@@ -3,21 +3,15 @@ package main
 import (
     "fmt"
     "testing"
+    "soma"
 )
 	
-
-func Soma(a, b int) int {
-    return a + b;
-}
-	
-
 func TestSoma(t *testing.T) {
     ans := Soma(5, 5)
     if ans != 10 {
         t.Errorf("Soma(5, 5) = %d; want 10", ans)
     }
 }
-
 
 func main() {
     fmt.Println(Soma(5,5))
